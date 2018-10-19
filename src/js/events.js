f291ff32c7d53d13fed4d4c718b68929214cc73c
Jamie OnePoly <jamie@onepolygon.co.uk>
2018-10-19 17:13:17 +0100

    Added an init fucntion to the onclick listener

--- src/js/events.js
+++ src/js/events.js
@@ -1,7 +1,10 @@
 document.querySelector("button").addEventListener("click", (e)=>
 {
+	init: function()
+	{
 	const animationComp = document.querySelector("#anim-property");
 
 	animationComp.setAttribute('dur', 10000);
 
-})
+	}
+});
