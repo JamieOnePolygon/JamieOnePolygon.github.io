1afe3459fe499651a5e1d0a0093b8b07b39e1251
Jamie <jamie@onepolygon.co.uk>
2018-11-21 10:47:29 +0000

    Updated event registration to be the correct method

--- src/js/eventhandler.js
+++ src/js/eventhandler.js
@@ -4,7 +4,7 @@ function setupListener()
 {
 	var marker = document.getElementById('hacker-attack-1');
 
-	marker.addListener('markerFound', function(event)
+	marker.addEventListener('markerFound', function(event)
 	{
 		alert("Found the Marker");
 	});
