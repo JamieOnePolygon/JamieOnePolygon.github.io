90ae773a6d579bec7f892d4192289c2ec2e415de
Jamie <jamie@onepolygon.co.uk>
2018-11-21 12:58:43 +0000

    changed from sphere to box

--- js/model-viewer/viewer.js
+++ js/model-viewer/viewer.js
@@ -4,7 +4,7 @@ var scene = new THREE.Scene();
 // Calculating the Aspect Ratio of the current window and creating a new
 // perspective camera.
 var aspect = window.innerWidth / window.innerHeight;
-var camera = new THREE.PerspectiveCamera(90, aspect, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 
 // Creating a new renderer which will render everything
 var renderer = new THREE.WebGLRenderer();
@@ -14,8 +14,8 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Now for the moment we are just going to add a sphere to render
-var geo = new THREE.SphereGeometry(1, 32, 32);
-var mat = new THREE.MeshBasicMaterial( {color: 0x00ff00});
+var geo = new THREE.BoxGeometry(1,1,1);
+var mat = new THREE.MeshBasicMaterial( {color: 0x800000 });
 
 var mesh = new THREE.Mesh(geo, mat);
 //Now that we have a mesh created, we are going to add it to the scene.
