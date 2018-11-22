4a24d0d919460860358efba633350fa930759d7b
Jamie <jamie@onepolygon.co.uk>
2018-11-22 13:51:30 +0000

    added a hemisphere light

--- js/model-viewer/viewer.js
+++ js/model-viewer/viewer.js
@@ -36,10 +36,14 @@ function setupScene()
 	renderer.setSize(window.innerWidth, window.innerHeight);
 
 	//Creating a directional light and adding it to the scene.
-	var dirLight = new THREE.DirectionalLight(0xFFDA82, 0.8);
+	var light = new THREE.DirectionalLight(0xFFDA82);
 	dirLight.position.x = 2;
 	dirLight.castShadow = true;
 	scene.add(dirLight);
+
+	light = new THREE.HemisphereLight(0xffffff, 0x444444);
+	light.position.set(0, 200, 0);
+	scene.add(light);
 }
 
 setupScene();
