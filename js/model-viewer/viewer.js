ab20ab3dbe2dad3179f366f00d4fe72fb7c800ac
Jamie <jamie@onepolygon.co.uk>
2018-11-26 17:28:23 +0000

    a

--- js/model-viewer/viewer.js
+++ js/model-viewer/viewer.js
@@ -202,7 +202,7 @@ function updateDescriptionAnnotation()
 	var vector = new THREE.Vector3(0, 50, 0);
 	var canvas = renderer.domElement;
 
-	position.project(camera);
+	vector.project(camera);
 
 	vector.x = Math.round((0.5 + vector.x / 2) * (canvas.width / window.devicePixelRatio));
 	vector.y = Math.round((0.5 - vector.y / 2) * (canvas.height / window.devicePixelRatio));
