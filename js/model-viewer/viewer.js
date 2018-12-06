f960264f047efd3ebfbfb697cd165e2de2aade4e
Jamie <jamie@onepolygon.co.uk>
2018-12-06 12:51:59 +0000

    trying out glitch effect

--- js/model-viewer/viewer.js
+++ js/model-viewer/viewer.js
@@ -103,7 +103,7 @@ function createObject()
 		object.mixer = new THREE.AnimationMixer(object);
 		mixers.push(object.mixer);
 
-		var action = object.mixer.clipAction(object.animations[1]);
+		var action = object.mixer.clipAction(object.animations[2]);
 		action.play();
 
 		scene.add(object);
